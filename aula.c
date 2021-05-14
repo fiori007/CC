fed39f75543f9759b3a5b4e1598a7df334107ea3
fiori007 <lucasfmm54@gmail.com>
2021-05-14 09:44:09 -0300

    Aula14/05-Arquivo

--- aula.c
+++ aula.c
@@ -6,14 +6,29 @@
 
 
 int main(){
-    char linha[50];
-    FILE *fp = fopen("batata.txt", "a");
-    while (fgets(linha, 50, fp) != NULL)
-        puts(linha);
-    printf("Entre com a linha: ");
-    scanf("%[^\n]s", linha);
-    //fputs(linha, fp);
-    fprintf(fp, "\n\n\n\n\n%s", linha);
+    char arq_origem[30], arq_destino[30];
+    char linha[100];
+    printf("entre com o nome do arq origem\n");
+    scanf("%s", arq_origem);
+    printf("entre com o nome do arq destino\n");
+    scanf("%s", arq_destino);
+
+    FILE *fp= fopen(arq_origem, "r");
+    FILE *fpDest= fopen(arq_destino, "w");
+
+    if (fp==NULL){
+        printf("erro na aberura do aqrquivo");
+        return -1;
+
+    }
+    while (fgets(linha, 100, fp)!=NULL){
+        char *tmp = strtok (linha, ",.;:?!\n\r\t");
+        while(tmp != NULL){
+            fprintf(fpDest, "%s\n", tmp);
+            tmp = strtok(NULL, ",.:;?!\n\r\t");
+        }
+    }
     fclose(fp);
+    fclose(fpDest);
     return 0;
 }
